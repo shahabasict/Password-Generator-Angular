05027101a3dc75609fe09bec3926bef907b5dd7f
Shahabas Aman(UST <285536@ust.com>
2024-09-09 15:24:45 +0530

    button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GeneratorComponent } from "./generator/generator.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GeneratorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/generator/generator.component.ts
@@ -0,0 +1,56 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-generator',
+  standalone: true,
+  imports: [FormsModule,CommonModule],
+  templateUrl: './generator.component.html',
+  styleUrl: './generator.component.css'
+})
+export class GeneratorComponent {
+
+generatedPassword:string = '';
+passwordLength:number=10;
+passwordStrength: number=0;
+includeUppercase:Boolean = true;
+includeLowercase:Boolean = true;
+includeNumbers:Boolean = true;
+includeSymbols:Boolean = true;
+
+generatePassword() {
+  const UpperChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const lowerChars = 'abcdefgjhijklmnopqrstuvwxyz';
+  const numberChars = '0123456789';
+  const symbols = '!@#$%^&*()_+<>?{}~';
+
+  let  password = '';
+  if(this.includeUppercase) password +=  UpperChars;
+  if(this.includeLowercase) password +=  lowerChars;
+  if(this.includeNumbers) password +=   numberChars;
+  if(this.includeSymbols) password +=   symbols;
+
+  this.generatedPassword = Array.from({length : this.passwordLength})
+    .map(()=>password.charAt(Math.floor(Math.random()*password.length)))
+    .join('');
+
+    this.calculateStrength()
+
+
+}
+  calculateStrength() {
+    this.passwordStrength = 0;
+    if (this.includeUppercase) this.passwordStrength++;
+    if (this.includeLowercase) this.passwordStrength++;
+    if (this.includeNumbers) this.passwordStrength++;
+    if (this.includeSymbols) this.passwordStrength++;
+
+    if (this.generatedPassword.length >= 12) {
+      this.passwordStrength++;
+    }
+
+  }
+
+
+}
